45481df7c40cdfa16b6bb0eb37615b3ecc575c55
Meghan Stang <mstang15@gmail.com>
2019-08-18 10:36:13 -0600

    Add helper method for displaying incomplete review flash message

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,14 +5,25 @@ class ReviewsController<ApplicationController
   end
 
   def create
-    item = Item.find(params[:item_id])
-    item.reviews.create(review_params)
-    redirect_to "/items/#{item.id}"
+    if field_empty?
+      item = Item.find(params[:item_id])
+      flash[:failure] = "Please fill in all fields in order to create a review."
+      redirect_to "/items/#{item.id}/reviews/new"
+    else
+      item = Item.find(params[:item_id])
+      item.reviews.create(review_params)
+      redirect_to "/items/#{item.id}"
+    end
   end
 
   private
 
   def review_params
     params.permit(:title,:content,:rating)
+  end
+
+  def field_empty?
+    params = review_params
+    params[:title].empty? || params[:content].empty? || params[:rating].empty?
   end
 end
