243d1af1b09fabad9f625ed0f35aa7cdd809b33d
Meghan Stang <mstang15@gmail.com>
2019-08-18 12:33:26 -0600

    Add route for adding item to cart

--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,6 @@ Rails.application.routes.draw do
   get "/reviews/:id/edit", to: "reviews#edit"
   patch "/reviews/:id", to: "reviews#update"
   delete "/reviews/:id", to: "reviews#destroy"
+
+  post "/cart/:item_id", to: "cart#add_item"
 end
