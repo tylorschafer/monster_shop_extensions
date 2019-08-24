44053cbbf587453fdd4c6626f4f89545625bedd3
Meghan Stang <mstang15@gmail.com>
2019-08-24 16:54:33 -0600

    Add relationship to item_orders through items

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,10 +1,16 @@
 class Merchant <ApplicationRecord
   has_many :items
-  
+  has_many :item_orders, through: :items
+
   validates_presence_of :name,
                         :address,
                         :city,
                         :state,
                         :zip
+
+
+  def no_orders?
+    item_orders.empty?
+  end
 
 end
