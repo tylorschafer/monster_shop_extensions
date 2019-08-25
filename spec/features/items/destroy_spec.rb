08e545a39fa0c9fc2eacf42861fbd5a8bb7c6f01
Meghan Stang <mstang15@gmail.com>
2019-08-25 11:40:59 -0600

    Add test for no button to delete when item has reviews

--- spec/features/items/destroy_spec.rb
+++ spec/features/items/destroy_spec.rb
@@ -15,5 +15,15 @@ RSpec.describe 'item delete', type: :feature do
       expect(current_path).to eq("/items")
       expect("item-#{chain.id}").to be_present
     end
+
+    it 'I cant delete item if that item has reviews' do
+      bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      review_1 = chain.reviews.create(title: "Great place!", content: "They have great bike stuff and I'd recommend them to anyone.", rating: 5)
+
+      visit "/items/#{chain.id}"
+
+      expect(page).to_not have_link("Delete Item")
+    end
   end
 end
