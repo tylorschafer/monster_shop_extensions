84d74b6801e6190422c15e92cb24cb8ebaf0114c
Meghan Stang <mstang15@gmail.com>
2019-09-02 11:26:07 -0600

    Add test for item count

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -14,16 +14,23 @@ describe Merchant, type: :model do
   end
 
   describe 'instance methods' do
+    before(:each) do
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    end
     it 'no_orders' do
-      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-
-      expect(meg.no_orders?).to eq(true)
+      expect(@meg.no_orders?).to eq(true)
 
       order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
-      item_order_1 = order_1.item_orders.create!(item: tire, price: tire.price, quantity: 2)
+      item_order_1 = order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
 
-      expect(meg.no_orders?).to eq(false)
+      expect(@meg.no_orders?).to eq(false)
+    end
+
+    it 'item_count' do
+      chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 30, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 22)
+
+      expect(@meg.item_count).to eq(2)
     end
 
   end
