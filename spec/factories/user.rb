62e5f21f5acef5d4790517611a285c4a37d0cf90
Tylor Schafer <tylorschafer@gmail.com>
2019-09-04 15:05:10 -0600

    Add better sequences to user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
-  factory :new_user do
+  factory :user do
     sequence(:name) {|x| "Name #{x}"}
     sequence(:address) {|x| "Address #{x}"}
     sequence(:city) {|x| "City #{x}"}
     sequence(:state) {|x| "State #{x}"}
-    sequence(:zip) {|x| x * 10000}
+    sequence(:zip) {|x| x.to_s.rjust(5, '0')}
     sequence(:email) {|x| "email #{x}"}
     sequence(:password) {|x| "password#{x}"}
   end
