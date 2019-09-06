9d978c8cf806e192c08b995ebd359bdf7d09944a
Tylor Schafer <tylorschafer@gmail.com>
2019-09-05 21:28:41 -0600

    Add status field to orders, defaults to pending

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order <ApplicationRecord
-  validates_presence_of :name, :address, :city, :state, :zip
+  validates_presence_of :name, :address, :city, :state, :zip, :status
 
   has_many :item_orders
   has_many :items, through: :item_orders
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -7,6 +7,7 @@ describe Order, type: :model do
     it { should validate_presence_of :city }
     it { should validate_presence_of :state }
     it { should validate_presence_of :zip }
+    it { should validate_presence_of :status }
   end
 
   describe "relationships" do
