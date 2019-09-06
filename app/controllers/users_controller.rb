3fe3286101c83ce56bc7d750526c4e4530094dae
Fenton Taylor <fentontaylor@gmail.com>
2019-09-05 18:34:10 -0600

    Fix registration so that it keeps the info if user doesnt fill in form right

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,15 @@ class UsersController <ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      flash[:success] = "Welcome, #{user.name}!"
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome, #{@user.name}!"
       redirect_to "/profile"
     else
-      flash[:error] = user.errors.full_messages.uniq.to_sentence
-      redirect_to '/register'
+      flash[:error] = @user.errors.full_messages.uniq.to_sentence
+      # redirect_to '/register'
+      render 'new'
     end
   end
 
