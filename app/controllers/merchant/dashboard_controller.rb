3b4cbb115302abc3603c3a1d3eab8e8ba6c47fe7
Fenton Taylor <fentontaylor@gmail.com>
2019-09-06 19:29:12 -0600

    Add test for admin can't visit cart
    Co-authored-by: Evette Telyas <50186721+evettetelyas@users.noreply.github.com>
    Co-authored-by: Tylor Schafer <3358870+tylorschafer@users.noreply.github.com>

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,7 +1,6 @@
 class Merchant::DashboardController < Merchant::BaseController
 
   def index
-    binding.pry
     @user = User.find(session[:user_id])
     @merchant = @user.merchant
     @pending_orders = @merchant.pending_orders
