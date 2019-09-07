61c925931a1646945924c053d35ac86fe70ccef3
Fenton Taylor <fentontaylor@gmail.com>
2019-09-06 19:11:56 -0600

    Add test for Merchant#pending_orders

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class Merchant::DashboardController < Merchant::BaseController
 
   def index
+    binding.pry
     @user = User.find(session[:user_id])
     @merchant = @user.merchant
     @pending_orders = @merchant.pending_orders
