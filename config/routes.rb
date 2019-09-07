4bb9d7e07a5878343777e767ca05d0ad3a56e222
Evette Telyas <etelyas@gmail.com>
2019-09-06 19:02:04 -0600

    complete merchant dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,7 @@ Rails.application.routes.draw do
   patch '/profile/edit_password', to: 'users#update_password'
 
   get '/merchant', to: 'merchant/dashboard#index', as: :merchant_dash
+  get '/merchant/items', to: 'merchant/dashboard#items'
   get '/admin', to: 'admin/dashboard#index', as: :admin_dash
   get '/admin/users', to: 'admin/dashboard#users'
 
