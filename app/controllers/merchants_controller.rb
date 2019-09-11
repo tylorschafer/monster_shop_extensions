1dcdbabc98550b76992f944aac792f86cc7b466b
Tylor Schafer <tylorschafer@gmail.com>
2019-09-10 21:09:48 -0600

    Refactor so disabled merchants no longer display in index and when trying to visit their show page you will get a 404 response

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,12 +1,13 @@
 class MerchantsController <ApplicationController
+  before_action :valid_merchant, only: [:show]
 
   def index
-    @merchants = Merchant.all
+    @merchants = Merchant.all.where(status: 0)
   end
 
   def show
     @merchant = Merchant.find(params[:id])
-    if current_user
+    if current_merchant?
       @works_here = @merchant.works_here?(current_user.merchant.id)
     end
   end
@@ -51,4 +52,7 @@ class MerchantsController <ApplicationController
     params.permit(:name,:address,:city,:state,:zip)
   end
 
+  def valid_merchant
+    render file: "/public/404" unless Merchant.find(params[:id]).status == 'enabled'
+  end
 end
