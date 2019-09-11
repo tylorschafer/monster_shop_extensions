7db93d8fb73ce001e2510de26f8e629399a3c79d
Tylor Schafer <tylorschafer@gmail.com>
2019-09-11 11:34:00 -0600

    Fix old tests

--- spec/features/items/new_spec.rb
+++ spec/features/items/new_spec.rb
@@ -4,16 +4,26 @@ RSpec.describe "Create Merchant Items" do
   describe "When I visit the merchant items index page" do
     before(:each) do
       @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+      @user = create(:user, role: 3, merchant_id: @brian.id)
+      visit '/login'
+
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+
+      within '#login-form' do
+        click_on 'Log In'
+      end
     end
 
     it 'I see a link to add a new item for that merchant' do
-      visit "/merchants/#{@brian.id}/items"
 
-      expect(page).to have_link "Add New Item"
+      visit "/merchant/items"
+
+      expect(page).to have_link "Add a New Item"
     end
 
     it 'I can add a new item by filling out a form' do
-      visit "/merchants/#{@brian.id}/items"
+      visit "/merchant/items"
 
       name = "Chamois Buttr"
       price = 18
@@ -21,21 +31,21 @@ RSpec.describe "Create Merchant Items" do
       image_url = "https://images-na.ssl-images-amazon.com/images/I/51HMpDXItgL._SX569_.jpg"
       inventory = 25
 
-      click_on "Add New Item"
+      click_on "Add a New Item"
 
       expect(page).to have_link(@brian.name)
-      expect(current_path).to eq("/merchants/#{@brian.id}/items/new")
-      fill_in :name, with: name
-      fill_in :price, with: price
-      fill_in :description, with: description
-      fill_in :image, with: image_url
-      fill_in :inventory, with: inventory
+      expect(current_path).to eq("/merchant/items/new")
+      fill_in 'Name', with: name
+      fill_in 'Price', with: price
+      fill_in 'Description', with: description
+      fill_in 'Image', with: image_url
+      fill_in 'Inventory', with: inventory
 
-      click_button "Create Item"
+      click_on "Submit"
 
       new_item = Item.last
 
-      expect(current_path).to eq("/merchants/#{@brian.id}/items")
+      expect(current_path).to eq("/merchant/items")
       expect(new_item.name).to eq(name)
       expect(new_item.price).to eq(price)
       expect(new_item.description).to eq(description)
@@ -44,15 +54,15 @@ RSpec.describe "Create Merchant Items" do
       expect(Item.last.active?).to be(true)
       expect("#item-#{Item.last.id}").to be_present
       expect(page).to have_content(name)
-      expect(page).to have_content("Price: $#{new_item.price}")
+      expect(page).to have_content("$#{new_item.price}")
       expect(page).to have_css("img[src*='#{new_item.image}']")
-      expect(page).to have_content("Active")
+      expect(page).to have_content("active")
       expect(page).to_not have_content(new_item.description)
-      expect(page).to have_content("Inventory: #{new_item.inventory}")
+      expect(page).to have_content("#{new_item.inventory}")
     end
 
     it 'I get an alert if I dont fully fill out the form' do
-      visit "/merchants/#{@brian.id}/items"
+      visit "/merchant/items"
 
       name = ""
       price = 18
@@ -60,18 +70,18 @@ RSpec.describe "Create Merchant Items" do
       image_url = "https://images-na.ssl-images-amazon.com/images/I/51HMpDXItgL._SX569_.jpg"
       inventory = ""
 
-      click_on "Add New Item"
+      click_on "Add a New Item"
 
-      fill_in :name, with: name
-      fill_in :price, with: price
-      fill_in :description, with: description
-      fill_in :image, with: image_url
-      fill_in :inventory, with: inventory
+      fill_in 'Name', with: name
+      fill_in 'Price', with: price
+      fill_in 'Description', with: description
+      fill_in 'Image', with: image_url
+      fill_in 'Inventory', with: inventory
 
-      click_button "Create Item"
+      click_button "Submit"
 
       expect(page).to have_content("Name can't be blank and Inventory is not a number")
-      expect(page).to have_button("Create Item")
+      expect(page).to have_button("Submit")
     end
   end
 end
