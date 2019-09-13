f874d4192c7711f09adac299f4a646a510a6ed2a
Tylor Schafer <tylorschafer@gmail.com>
2019-09-12 21:33:06 -0600

    Add model testing for address table

---
+++ spec/models/address_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Address do
+  describe 'validations' do
+    it {should validate_presence_of :nickname}
+    it {should validate_presence_of :address}
+    it {should validate_presence_of :city}
+    it {should validate_presence_of :state}
+    it {should validate_presence_of :zip}
+  end
+
+  describe 'relationships' do
+    it belongs_to :user
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,7 @@ describe User, type: :model do
 
   describe "relationships" do
     it {should have_many :orders}
+    it {should have_many :addresses}
   end
 
   describe "model methods" do
