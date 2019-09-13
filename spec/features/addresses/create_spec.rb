eef75bf4cf3c77d222805c9e19e91d2df62ba2b0
Tylor Schafer <tylorschafer@gmail.com>
2019-09-12 21:55:19 -0600

    Add new user creates new address test

---
+++ spec/features/addresses/create_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe 'new user creation' do
+  before :each do
+    @name = 'Bobb'
+    @address = '234 A st'
+    @city = 'Wonderland'
+    @state = 'CA'
+    @zip = 90345
+    @email = 'bobb@gmail.com'
+    @password = 'supersafe'
+  end
+  describe 'the address the user enters into the new user form' do
+    it 'is saved into the addresses database and given a default nickname of Home' do
+      visit '/'
+
+      within '.login-options' do
+        click_link 'Sign Up'
+      end
+
+      fill_in 'Name', with: @name
+      fill_in 'Address', with: @address
+      fill_in 'City', with: @city
+      fill_in 'State', with: @state
+      fill_in 'Zip', with: @zip
+      fill_in 'Email', with: @email
+      fill_in 'Password', with: @password
+      fill_in 'Password confirmation', with: @password
+
+      click_on 'Submit'
+
+      new_address = Address.last
+      new_user = User.last
+
+      expect(new_address.address).to eq(@address)
+      expect(new_address.address).to eq(@city)
+      expect(new_address.address).to eq(@state)
+      expect(new_address.address).to eq(@zip)
+      expect(new_address.nickname).to eq('home')
+      expect(new_address.user_id).to eq(new_user.id)
+    end
+  end
+end
