da12e185b8ad3156c41c49d53abd4ceb0cd06368
Tylor Schafer <tylorschafer@gmail.com>
2019-09-14 15:47:46 -0600

    Revert address nickname uniqueness

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,5 @@
 class Address < ApplicationRecord
-  validates_presence_of :address, :city, :state, :zip
-  validates :nickname, presence: true, uniqueness: true
+  validates_presence_of :address, :city, :state, :zip, :nickname
 
   belongs_to :user
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe Address do
   describe 'validations' do
     it {should validate_presence_of :nickname}
-    it {should validate_uniqueness_of(:nickname)}
     it {should validate_presence_of :address}
     it {should validate_presence_of :city}
     it {should validate_presence_of :state}
