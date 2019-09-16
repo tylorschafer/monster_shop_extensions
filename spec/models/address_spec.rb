550d20f051ebbd5a17a6d87f8acd747e7f1e8680
Tylor Schafer <tylorschafer@gmail.com>
2019-09-15 22:18:05 -0600

    Add model test for address has_order method

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -13,4 +13,17 @@ describe Address do
     it {should belong_to :user}
     it {should have_many :orders}
   end
+
+  describe 'instance methods' do
+    before :each do
+
+    end
+    it '#has_order?' do
+      user = create(:user)
+      address = user.addresses[0]
+      order_1 = user.orders.create!(name: 'Meg', address: address)
+
+      expect(address.has_order?).to eq(true)
+    end
+  end
 end
